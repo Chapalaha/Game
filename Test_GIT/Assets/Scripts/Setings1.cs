1c332e1e385647e3b8bd85568f30789f6c43560e
gg133 <133chess@gmail.com>
2022-05-04 12:38:22 +0300

    pochti last

---
+++ Test_GIT/Assets/Scripts/Setings1.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Setings1 : MonoBehaviour
+{
+    public void Gg()
+    {
+        SceneManager.LoadScene("MenuSettigs");
+    }
+
+
+}
